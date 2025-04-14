6e6f3ab8e872924e151bdf25b4d6a704f9bc6ebf
Michael Todd <michaeltodd981@gmail.com>
2025-04-14 12:08:18 +0200

    Added 'Push Camera' instead of Drag on world

--- Assets/Scripts/Core/Camera/CameraController.cs
+++ Assets/Scripts/Core/Camera/CameraController.cs
@@ -71,40 +71,51 @@ public class CameraController : MonoBehaviour
         }
     }
 
-    void HandleMovementInput() {
-        if(Input.GetKey(KeyCode.LeftShift)) {
+        void HandleMovementInput() {
+        Vector3 direction = Vector3.zero;
+    
+        float edgeThreshold = 20f; 
+        float maxSpeedMultiplier = 100f; 
+    
+        if (Input.mousePosition.x >= Screen.width - edgeThreshold) {
+            float proximity = (Input.mousePosition.x - (Screen.width - edgeThreshold)) / edgeThreshold;
+            direction += transform.right * Mathf.Clamp(proximity * maxSpeedMultiplier, 0, maxSpeedMultiplier);
+        }
+        if (Input.mousePosition.x <= edgeThreshold) {
+            float proximity = (edgeThreshold - Input.mousePosition.x) / edgeThreshold;
+            direction -= transform.right * Mathf.Clamp(proximity * maxSpeedMultiplier, 0, maxSpeedMultiplier);
+        }
+    
+        if (Input.mousePosition.y >= Screen.height - edgeThreshold) {
+            float proximity = (Input.mousePosition.y - (Screen.height - edgeThreshold)) / edgeThreshold;
+            direction += transform.forward * Mathf.Clamp(proximity * maxSpeedMultiplier, 0, maxSpeedMultiplier);
+        }
+        if (Input.mousePosition.y <= edgeThreshold) {
+            float proximity = (edgeThreshold - Input.mousePosition.y) / edgeThreshold;
+            direction -= transform.forward * Mathf.Clamp(proximity * maxSpeedMultiplier, 0, maxSpeedMultiplier);
+        }
+    
+        if (Input.GetKey(KeyCode.LeftShift)) {
             movementSpeed = fastSpeed;
         } else {
             movementSpeed = normalSpeed;
         }
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            newPosition += (transform.forward * movementSpeed);
-        }
-        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            newPosition += (transform.forward * -movementSpeed);
-        }
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            newPosition += (transform.right * movementSpeed);
-        }
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            newPosition += (transform.right * -movementSpeed);
-        }
-
-        if(Input.GetKey(KeyCode.Q)) {
+    
+        newPosition += direction * movementSpeed * Time.deltaTime;
+    
+        if (Input.GetKey(KeyCode.Q)) {
             newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
         }
-
-        if(Input.GetKey(KeyCode.E)) {
+        if (Input.GetKey(KeyCode.E)) {
             newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
         }
-        
-        if(Input.GetKey(KeyCode.R)) {
+        if (Input.GetKey(KeyCode.R)) {
             newZoom += zoomAmount;
         }
-        if(Input.GetKey(KeyCode.F)) {
+        if (Input.GetKey(KeyCode.F)) {
             newZoom -= zoomAmount;
         }
-
+    
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
